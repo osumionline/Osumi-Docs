7f1c0b8ce240a78ebd3e8ff6c5c7ace21987d82a
Iñigo <inigo.gorosabel@gmail.com>
2020-03-24 16:09:58 +0100

    Actualizo OFW (5.0.3) con todos los cambios que conlleva

--- app/template/partials/common/footer.php
+++ app/template/partials/common/footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <a href="https://github.com/igorosabel/Osumi-Framework/releases/tag/v<?php echo Base::getVersion() ?>" class="footer-version">Versión: <?php echo Base::getVersion() ?></a>
+  <a href="https://github.com/igorosabel/Osumi-Framework/releases/tag/v<?php echo OTools::getVersion() ?>" class="footer-version">Versión: <?php echo OTools::getVersion() ?></a>
   <a href="https://github.com/igorosabel/Osumi-Framework">Github</a>
   <a href="https://twitter.com/igorosabel">Twitter</a>
 </footer>
